986ac65097f741d0f608b40edb7b9ce6dcacfb37
helloankur <helloankur.2009@gmail.com>
2021-08-30 22:07:47 +0800

    Update Run

--- detect.py
+++ detect.py
@@ -298,6 +298,7 @@ if __name__ == "__main__":
         cv2.imshow('img',img)
         cv2.waitKey()
         shutil.rmtree('runs',ignore_errors=True)
+        print("remove saved images")
         # os.removedirs('runs')
         time.sleep(1)
 
